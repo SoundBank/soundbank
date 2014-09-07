edef5eada24e459d7ca5a42859c41416c6ddef26
SoundBank <cat.urandom@gmail.com>
2014-09-07 19:48:37 +0100

    app js Hacks 07

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap.min
-//= require jquery
 //= require jquery.touchSwipe.min
 //= require jquery.velocity.min
+//= require jquery
 //= require turbolinks
+//= require bootstrap.min
