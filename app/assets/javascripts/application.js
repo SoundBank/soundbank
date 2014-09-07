504fb1f1f0bedd353156e4021438ee3ca61cb392
SoundBank <cat.urandom@gmail.com>
2014-09-07 14:23:27 +0100

    Reordered application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require jquery-1.10.2.min.js
+//= require jquery_ujs
 //= require jquery.velocity.min.js
 //= require jquery.touchSwipe.min.js
 //= require turbolinks
