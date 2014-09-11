c2483261567ae1a59e9fdf5947fc6d95c62fd209
SoundBank <cat.urandom@gmail.com>
2014-09-11 22:21:09 +0100

    User scaffold added

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907220840) do
+ActiveRecord::Schema.define(version: 20140911211313) do
 
   create_table "supports", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
